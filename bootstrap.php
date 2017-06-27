09e42fee1e463f0cbadcb256d4d72e100b8434c2
Jake Bentvelzen <jake@silverstripe.com.au>
2017-06-27 19:52:12 +1000

    Add support for DataObject::get() calls

--- bootstrap.php
+++ bootstrap.php
@@ -3,4 +3,15 @@
 /**
  * This file is required to setup the autoloader
  */
-require_once(dirname(__FILE__).'/../framework/core/Core.php');
+$CORE_PATH = dirname(__FILE__).'/../framework/core';
+require_once($CORE_PATH.'/TempPath.php');
+
+// Copy-pasted from framework/Constants.php
+/*$candidateBasePath = rtrim(dirname(dirname(dirname(__FILE__))), DIRECTORY_SEPARATOR);
+if($candidateBasePath == '') $candidateBasePath = DIRECTORY_SEPARATOR;
+define('BASE_PATH', $candidateBasePath);
+
+// Override silverstripe-cache username to 'phpstan'
+define('TEMP_FOLDER', getTempParentFolder(BASE_PATH) . DIRECTORY_SEPARATOR . 'phpstan');*/
+
+require_once($CORE_PATH.'/Core.php');
---
+++ src/DataObjectGetStaticReturnTypeExtension.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types = 1);
+
+namespace SilbinaryWolf\SilverstripePHPStan;
+
+use PHPStanVendor\PhpParser\Node\Expr\StaticCall;
+use PHPStanVendor\PhpParser\Node\Expr\PropertyFetch;
+use PHPStanVendor\PhpParser\Node\Expr\Variable;
+use PHPStanVendor\PhpParser\Node\Scalar\String_;
+
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Analyser\Scope;
+use PHPStan\Type\Type;
+use PHPStan\Type\ObjectType;
+
+// Silverstripe
+use DataList;
+use DataObject;
+
+class DataObjectGetStaticReturnTypeExtension implements \PHPStan\Type\DynamicStaticMethodReturnTypeExtension {
+    public static function getClass(): string
+    {
+        return DataObject::class;
+    }
+
+    public function isStaticMethodSupported(MethodReflection $methodReflection): bool
+    {
+        $name = $methodReflection->getName();
+        return $name === 'get' || $name === 'get_one' || $name === 'get_by_id';
+    }
+
+    public function getTypeFromStaticMethodCall(MethodReflection $methodReflection, StaticCall $methodCall, Scope $scope): Type
+    {
+        $name = $methodReflection->getName();
+        switch ($name) {
+            case 'get':
+                if (count($methodCall->args) !== 0) {
+                    // Handle DataObject::get('Page')
+                    $arg = $methodCall->args[0];
+                    if ($arg instanceof String_) {
+                        return new DataListType(DataList::class, new ObjectType($arg->value));
+                    }
+                    return $methodReflection->getReturnType();
+                }
+                // Handle Page::get() / self::get()
+                $callerClass = $methodCall->class->toString();
+                if ($callerClass === 'static') {
+                    return $methodReflection->getReturnType();
+                }
+                if ($callerClass === 'self') {
+                    $callerClass = $scope->getClassReflection()->getName();
+                }
+
+                return new DataListType(DataList::class, new ObjectType($callerClass));
+            break;
+
+            case 'get_one':
+            case 'get_by_id':
+                if (count($methodCall->args) === 0) {
+                    return $methodReflection->getReturnType();
+                }
+                $arg = $methodCall->args[0]->value;
+                if ($arg instanceof String_) {
+                    return new ObjectType($arg->value);
+                }
+                /*if ($arg instanceof PropertyFetch) {
+                    $vars = $scope->getVariableTypes();
+                   var_dump($vars);
+                    var_dump($arg);  
+                }*/
+                return $methodReflection->getReturnType();
+            break;
+        }
+        return $methodReflection->getReturnType();
+    }
+}
